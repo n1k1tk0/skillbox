c2ba0fa31885a4d778761c091e8d705164dbf191
Nikita Vladimirov <vladimirovnn81@gmail.com>
2023-10-25 22:57:03 +0300

    test2

--- m16/ViewController.swift
+++ m16/ViewController.swift
@@ -8,11 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
+    override func viewDidLoad() { super.viewDidLoad() }
 }
 
