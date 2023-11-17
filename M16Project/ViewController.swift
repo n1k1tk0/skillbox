5bc15849305b14969d62911e53c79acb687ffce7
Nikita Vladimirov <vladimirovnn81@gmail.com>
2023-11-18 00:41:55 +0300

    fix

--- M16Project/ViewController.swift
+++ M16Project/ViewController.swift
@@ -7,7 +7,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-        view.layer.borderColor = UIColor.green.cgColor
     }
 }
 
