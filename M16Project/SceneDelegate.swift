eed8db5e0aec4f58d7ccbe9941d1d79a3f5217df
Nikita Vladimirov <vladimirovnn81@gmail.com>
2023-11-20 00:04:30 +0300

    update SceneDelegate

--- M16Project/SceneDelegate.swift
+++ M16Project/SceneDelegate.swift
@@ -13,8 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: scene)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let _ = UIWindow(windowScene: windowScene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
