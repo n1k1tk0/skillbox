17017cfc5f49ce7f243a1d55034489616f6593f0
Nikita Vladimirov <vladimirovnn81@gmail.com>
2023-11-23 16:25:46 +0300

    setup activity indicator

--- M16Project/ViewController.swift
+++ M16Project/ViewController.swift
@@ -6,12 +6,25 @@ class ViewController: UIViewController {
 
     private lazy var activityIndicator: UIActivityIndicatorView = {
         let view = UIActivityIndicatorView(style: .large)
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        view.backgroundColor = .white
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(activityIndicator)
+    }
+    
+    private func setupConstraints() {
+        activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
