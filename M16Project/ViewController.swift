ba479c9dcbbf34fe128876d869eb169ed6b7c58a
Nikita Vladimirov <vladimirovnn81@gmail.com>
2023-11-28 23:09:59 +0300

    update VC

--- M16Project/ViewController.swift
+++ M16Project/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
         
         setupViews()
         setupConstraints()
